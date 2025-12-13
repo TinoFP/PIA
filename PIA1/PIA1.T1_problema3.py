798bf1890c50490835c6a60fefb20b5ad7588d77
TinoFP <profefp.angel@gmail.com>
2025-12-13 20:30:59 +0100

    Problema 3 terminado

--- PIA1/PIA1.T1_problema3.py
+++ PIA1/PIA1.T1_problema3.py
@@ -1,20 +1,18 @@
 # Módulo PIA
 # Tarea 1
-# Ejercicio 2. Problema 3: Conjuntos
+# Ejercicio 2. Problema 3. Trabajo con conjuntos 
 #
 # Alumno: Angel Tinoco
 #
 
 
 #########
-# EJEMPLO DE USO
+# Pide al usuario usuario valores para el primer conjunto
 
-# Pide al usuario usuario valores para el primer conj
-
-usuario = input ("Introduce varios números para el conjunto 1, separados por espacios.")
+usuario = input ("Introduce varios números para el conjunto 1, separados por espacios: ")
 conjunto1 = set(map(int,usuario.split()))
 
-usuario = input ("Introduce varios números para el conjunto 1, separados por espacios.")
+usuario = input ("Introduce varios números para el conjunto 2, separados por espacios: ")
 conjunto2 = set(map(int,usuario.split()))
 
 # Calcular la intersección
@@ -22,13 +20,13 @@ interseccion = conjunto1.intersection(conjunto2)
 print("\nIntersección (están en ambos conjuntos):", interseccion)
 
 
-# Calcular la unión
+# Calcular la unión de conjuntos
 union = conjunto1.union(conjunto2)
-print("\nUnión (se añaden los elementos de ambos conjuntos pero sin repetidos):", union)
+print("\nUnión (se añaden los elementos de ambos conjuntos pero sin duplicados):", union)
 
 
 # Calcular la diferencia simétrica
 diferencia_simetrica = conjunto1.symmetric_difference(conjunto2)
-print("\nDiferencia Simétrica (elementos en uno u otro, pero no en ambos):", diferencia_simetrica)
+print("\nDiferencia Simétrica (elementos en uno u otro, pero no en ambos):", diferencia_simetrica) 
 
 
