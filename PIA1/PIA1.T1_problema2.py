8501cc5670be35afa9360c58e1561c7962aa2675
TinoFP <profefp.angel@gmail.com>
2025-12-13 20:13:01 +0100

    Problema 2 terminado

--- PIA1/PIA1.T1_problema2.py
+++ PIA1/PIA1.T1_problema2.py
@@ -1,26 +1,72 @@
 # Módulo PIA
 # Tarea 1
-# Ejercicio 2. Problema 2: Frecuencia de las palabtas
+# Ejercicio 2. Problema 2. Frecuencia de palabras en un texto.
 #
 # Alumno: Angel Tinoco
 #
 
+# Librerias
+import os
+import string
 
-def contar_frecuencia_palabras(texto):
-    # Convertir el texto a minúsculas
-    texto = texto.lower()
 
-    # Separar el texto completo en palabras, usando split con " " lo hace rápidamente
-    lista_palabras = texto.split()
+def frecuencia_palabras(iPalabras, iFichero):
+    # CHEQUEOS PREVIOS ---------------------------
+    if iPalabras == "":
+        print("ERROR: No has indicado ninguna palabra para analizar su frecuencia.")
+        exit (1)
+    
 
-    # Usando una lista tipo diccionario para almacenar cada palabra y su frecuencia
-    dic_frecuencia = {}
-    for palabra in lista_palabras:
-        if palabra in dic_frecuencia:
-            dic_frecuencia[palabra] += 1
-        else:
-            dic_frecuencia[palabra] = 1
 
+    # Analiza si existe el fichero
+    if os.path.exists(iFichero):
+
+        # COMIENZA EL PROCESAMIENTO -------------------
+     
+        # Usando una lista tipo diccionario para almacenar cada palabra y su frecuencia
+        dic_frecuencia = {}
+
+        # Lee el fichero y lo procesa
+        try:
+            with open(iFichero,"r") as f:
+                
+                # Convertir las palabras a analizar minúsculas
+                iPalabras= iPalabras.lower()
+                contenido_fich = f.read().lower()
+
+                # quita signos de puntuacion
+                iPalabras = iPalabras.translate(str.maketrans("","",string.punctuation))
+                contenido_fich = contenido_fich.translate(str.maketrans("","",string.punctuation))
+
+                # Separar el texto completo en palabras, usando split con " " lo hace rápidamente
+                lista_palabras = iPalabras.split()
+                contenido_fich = contenido_fich.split()
+
+                # Inicializa el diccionario. Doy de alta todas las palabras buscadas, si no aparecen
+                # en el texto, se pone un 0.
+                for palabra in lista_palabras:
+                    dic_frecuencia[palabra] = 0
+
+               
+                # Lee el contenido del texto.
+                for palabra in contenido_fich:
+                    # busca cada palabra
+                    if palabra in lista_palabras:
+                        # Si encuentra la palabra, aumenta la frecuencia
+                        if palabra in dic_frecuencia:
+                            dic_frecuencia[palabra] += 1
+
+
+        except PermissionError:
+            print(f"ERROR: No tienes permisos para abrir el fichero '{iFichero}'.")
+        except IOError as e:
+            print(f"Ocurrió un error de E/S inesperado: {e}")
+    else:
+        print ("ERROR: El fichero "+iFichero+" no existe.")
+        exit(2)
+    # Fin IF
+
+    
     # Ordenar el diccionario
     dic_frecuencia_ordenado = dict(sorted(dic_frecuencia.items()))
 
@@ -34,6 +80,15 @@ def contar_frecuencia_palabras(texto):
 #####################
 # EJEMPLO DE USO
         
-# Pide un texto al usuario
-texto = input("Escribe texto para analizar: ")
-contar_frecuencia_palabras(texto)
+# Pide una lista de palabras al usuario
+palabras = input("Escribe las palabras que quieras contar: ")
+fichero = input("Indica la ruta del fichero a procesar: ")
+print ("\n\n")
+
+# EJEMPLO PRUEBAS
+# palabras = "prueba 2 fichero de texto"
+# fichero = "/home/tino/Escritorio/GIT/PIA/PIA1/texto_problema2.txt"
+
+# Ejecuta la función
+frecuencia_palabras(palabras, fichero)
+
